2a1b1c2cb0afb3d55b6335c52e1a6e2be4d04e34
jeevitha-jr <jeevitha.jr@exathought.com>
2023-04-21 11:01:29 +0530

    build gradle version update

--- BuildAndDeployAndroidInternalTestersRelease.sh
+++ BuildAndDeployAndroidInternalTestersRelease.sh
@@ -3,5 +3,5 @@ flutter pub global run rename --appname "carparts.com"
 cd android
 # fastlane bump_version_code
 flutter clean
-flutter build appbundle --build-number=1
+flutter build appbundle --build-number=2
 fastlane deploy_internal_testers_release package_name:com.carparts.app
