73abb822fb81c6850252e84b2d7d73fbb51776cc
VaishakMB08 <vaishak.mb@exathought.com>
2023-05-17 11:27:42 +0530

    push notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
 import 'dart:io';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'src/web_view_stack.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // LocalNotificationPlugin.instance.backgroundDisplay(message);
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
+
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
