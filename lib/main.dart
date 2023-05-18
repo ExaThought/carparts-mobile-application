740c96c2254fcfe2ce1c27707d3b52d35390c8bb
VaishakMB08 <vaishak.mb@exathought.com>
2023-05-18 13:11:35 +0530

    working android push notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,37 @@
 import 'dart:io';
+import 'package:carparts/local_notification_plugin.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'firebase_options.dart';
 import 'src/web_view_stack.dart';
 
-void main() {
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  LocalNotificationPlugin.instance.backgroundDisplay(message);
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+  FirebaseMessaging message = FirebaseMessaging.instance;
+  NotificationSettings settings = await message.requestPermission(
+          alert: true,
+          announcement: true,
+          badge: true,
+          carPlay: false,
+          criticalAlert: false,
+          provisional: false,
+          sound: true,
+        );
+
+  print(await FirebaseMessaging.instance.getToken());
+
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -38,9 +65,43 @@ class _WebViewAppState extends State<WebViewApp> {
   @override
   void initState() {
     super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      LocalNotificationPlugin.instance
+          .initialize();
+    });
+    /*
+    *On click of notification, it opens app from terminated state
+    * It is also important so that foreground works and used when app is in background or terminated or closed
+    */
+    FirebaseMessaging.instance.getInitialMessage().then((message) {
+      /*
+     *For routing of page from terminated state to be handled here
+     */
+      if (message != null) {
+        LocalNotificationPlugin.instance.backgroundDisplay(message);
+      }
+    });
+
+    /*
+    *When app is in foreground that is running 
+    */
+    FirebaseMessaging.onMessage.listen((message) {
+      LocalNotificationPlugin.instance.display(message);
+    });
+    /*
+      *When app is open and to make sure for onclick of notification that, 
+      *It is routed to different page
+      *Works when user taps on the notification 
+      *Works only when app is in background but opened and user taps
+      */
+
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      LocalNotificationPlugin.instance.backgroundDisplay(message);
+    });
     controller = WebViewController()
       ..loadRequest(
-        Uri.parse('https://www.carparts.com/?force_can=1'),
+        Uri.parse('https://www.carparts.com/'),
       );
   }
 
