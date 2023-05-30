c0377517a695b3d79911cee7cf41979f3f1183c6
VaishakMB08 <108452620+VaishakMB08@users.noreply.github.com>
2023-05-30 10:10:42 +0530

    Push notification for android platform (#13)
    * working android push notification
    
    * fixed push notification
    
    * push notification

---
+++ lib/local_notification_plugin.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotificationPlugin {
+  static LocalNotificationPlugin instance = LocalNotificationPlugin();
+  static final FlutterLocalNotificationsPlugin notificationPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  void initialize() {
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: AndroidInitializationSettings("@mipmap/ic_launcher"),
+      iOS: IOSInitializationSettings(
+        requestSoundPermission: false,
+        requestBadgePermission: false,
+        requestAlertPermission: false,
+      ),
+    );
+    notificationPlugin.initialize(initializationSettings);
+  }
+
+  void display(RemoteMessage message) async {
+    try {
+      final id = DateTime.now().microsecondsSinceEpoch ~/ 100000000000;
+      const NotificationDetails notificationDetails = NotificationDetails(
+        android: AndroidNotificationDetails(
+            "default_notification_channel_id", "vikrayChannel",
+            channelDescription: "These is Vikray channel",
+            importance: Importance.max,
+            priority: Priority.high),
+      );
+/*
+*Creates the notification channel when it runs first time
+*Call back for on select notification
+*/
+      await notificationPlugin.show(id, message.notification!.title,
+          message.notification!.body, notificationDetails,
+          payload: "");
+    } catch (error) {
+      rethrow;
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,33 @@
+import 'package:carparts/local_notification_plugin.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'firebase_options.dart';
 import 'src/web_view_stack.dart';
 import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
-void main() {
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+  FirebaseMessaging message = FirebaseMessaging.instance;
+  NotificationSettings settings = await message.requestPermission(
+    alert: true,
+    announcement: true,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -38,6 +61,34 @@ class _WebViewAppState extends State<WebViewApp> {
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      LocalNotificationPlugin.instance.initialize();
+    });
+    /*
+    *On click of notification, it opens app from terminated state
+    * It is also important so that foreground works and used when app is in background or terminated or closed
+    */
+    FirebaseMessaging.instance.getInitialMessage().then((message) {
+      /*
+     *For routing of page from terminated state to be handled here
+     */
+    });
+
+    /*
+    *When app is in foreground that is running 
+    */
+    FirebaseMessaging.onMessage.listen((message) {
+      LocalNotificationPlugin.instance.display(message);
+    });
+    /*
+      *When app is open and to make sure for onclick of notification that, 
+      *It is routed to different page
+      *Works when user taps on the notification 
+      *Works only when app is in background but opened and user taps
+      */
+
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {});
+
     controller = WebViewController();
     if (controller.platform is WebKitWebViewController) {
       (controller.platform as WebKitWebViewController)
